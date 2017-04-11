286d8ae6eac170baed104edd169ba679a9b93303
Mazzeo Alejandro <ale.exactas@gmail.com>
2017-04-11 18:43:28 -0300

    Hoshen en pruebas

--- PruebaC.c
+++ PruebaC.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include "percolar.h"
 
-#define N		32
+#define N		124
 
 int main()
 {
 	int n=N, red[n*n];
 	float proba=0.5;
 	int s=260572;
+	int i=0,j=0;
 
-	llenar(red, n, proba, &s);
-	imprimir(red, n);
-	hoshen(red, n);
-	imprimir(red, n);
+	for (i=0;i<27;i++)
+	{
+		for (j=0;j<1000;j++)
+		{
+			llenar(red, n, proba, &s);
+			hoshen(red, n);
+		}
+		printf("*");
+		fflush(stdout);
+	}
+	//imprimir(red, n);
 }
