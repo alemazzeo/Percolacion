e39f03af87e4c467dcad039da45c9eea370a6e42
Mazzeo Alejandro <ale.exactas@gmail.com>
2017-04-11 20:47:14 -0300

    Prueba de python estancada. Requiere modificar percolar.c

--- Prueba.py
+++ Prueba.py
@@ -1,38 +1,36 @@
 import ctypes as C
 import numpy as np
 
-N = 30
+N = 124
 prob = 0.5
-semilla = 260572
+semilla = 26572
 
 red = np.zeros(N, dtype=C.c_int)
 
 class Percolar():
-	def __init__(self, lado, prob, semilla):
-		self.N = lado
+	def __init__(self, N, prob, semilla):
+		self.N = N
 		self.prob = prob
-		self.semilla = semilla
+		self.semilla = C.c_int(semilla)
 		
 		self.percolar = C.CDLL('./libpercolar.so')
 		self.intp = C.POINTER(C.c_int)
 
-		self.red = np.zeros(self.N, dtype=C.c_int)
+		self.red = np.zeros(self.N**2, dtype=C.c_int)
 
-		self.percolar.llenar(self.red.ctypes.data_as(self.intp), C.c_int(len(self.red)), 
-						C.c_float(self.proba), C.byref(self.semilla))
+		self.percolar.llenar(self.red.ctypes.data_as(self.intp), C.c_int(self.N), 
+						C.c_float(self.prob), C.byref(self.semilla))
 
 	def llenar(self, prob, semilla):
 		self.prob = prob
-		self.semilla = semilla
-		self.red = np.zeros(self.N, dtype=C.c_int)
+		self.semilla = C.c_int(semilla)
+		self.red = np.zeros(self.N**2, dtype=C.c_int)
 
-		self.percolar.llenar(self.red.ctypes.data_as(self.intp), C.c_int(len(self.red)), 
-						C.c_float(self.proba), C.byref(self.semilla))
+		self.percolar.llenar(self.red.ctypes.data_as(self.intp), C.c_int(self.N), 
+						C.c_float(self.prob), C.byref(self.semilla))
 
 	def hoshen(self):
-		self.percolar.hoshen(red.ctypes.data_as(intp), C.c_int(len(red)))
-
-print("E")
+		self.percolar.hoshen(red.ctypes.data_as(self.intp), C.c_int(self.N))
 
 mired = Percolar(N, prob, semilla)
-mired.llenar(prob, semilla+1)
+mired.hoshen()
